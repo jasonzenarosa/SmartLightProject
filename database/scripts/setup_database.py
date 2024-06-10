9cd1f19e8128767a0180144a9667ca831ed2ff62
Jason Zenarosa <jasonzenarosa@gmail.com>
2024-06-10 16:48:34 -0700

    create database setup script

---
+++ database/scripts/setup_database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# Connect to the database (or create it if it doesn't exist)
+connection = sqlite3.connect('SmartLight.db')
+
+# Create a cursor object to execute SQL commands
+cursor = connection.cursor()
+
+cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Logs (
+               log_id       INTEGER PRIMARY KEY AUTOINCREMENT,
+               light_on     INTEGER NOT NULL,
+               datetime     TEXT NOT NULL,
+               method       TEXT NOT NULL
+            )
+''')
+
+# Commit the changes and close the connection
+cursor.close()
+connection.commit()
+connection.close()
